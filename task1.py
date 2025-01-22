493bcb05eb9816746b58d6f439d82520f4dcf046
Semyon <s.golokolos@school.nsu.ru>
2025-01-22 08:05:26 +0700

    Исправил и довёл до ума первое

--- task1.py
+++ task1.py
@@ -5,17 +5,24 @@
 # Общее количество символов (включая пробелы).
 # Выведите полученную статистику на экран.
 
+s = 0
 with open('task1.txt', encoding='utf-8') as f:
     ln = f.read()
 
 def st(A):
-    A = str(A.split('—'))
+    A = str(A.split(' '))
     i, n = 0, 1
     while A[i] != ']':
         if A[i] == '\\'  and A[i+1] == 'n':
             n += 1
         i += 1
     return n
-ln = ln.split
+print(st(ln))
 
-print(st(ln), k, len(ln))
+ln = ln.split()
+k = len(ln)
+print(k)
+
+for x in range(len(ln)):
+    s += len(ln[x])
+print(s)
--- test.py
+++ test.py
@@ -2,7 +2,4 @@ with open('task3.txt', encoding='utf-8') as f:
     ln = f.read()
 ln = ln.lower()
 ln = ln.split()
-print(ln[5])
-if ln[5].endswith('.'):
-    ln[5] = ln[5].replace('.', '')
-print(ln[5])
+print(ln)
